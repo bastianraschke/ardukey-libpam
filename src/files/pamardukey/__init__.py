a9038bf525fa4b34a97a60303609a656c575c304
Philipp Meisberger <team@pm-codeworks.de>
2015-01-06 17:05:05 +0100

    Added preparation for bash completion; Package now uses python 2

---
+++ src/files/pamardukey/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+ArduKey PAM implementation
+@author Philipp Meisberger <team@pm-codeworks.de>
+
+Copyright 2015 Philipp Meisberger
+All rights reserved.
+"""
+
+__version__ = '1.0'
+__copyright__ = 'Philipp Meisberger'
+__organization__ = 'Bastian Raschke'
+__license__ = 'Simplified BSD License'
+__authors__ = ['Philipp Meisberger']
